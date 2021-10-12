6e786fd15efe182e280a435f2264e5d6393e202e
Michael <maew711@gmail.com>
2021-10-11 23:18:27 -0400

    Update README.md

--- movies-etl/README.md
+++ movies-etl/README.md
@@ -1,9 +1,44 @@
 ## ETL (Extract, Transform and Load) Process
-Build automated data pipeline for movies data
+
+### Overview
+Refactor [movies-etl notebook](movies-etl.ipynb) for Module 8 exercises into an automated data pipeline.
+
+![](images/film_strip.png)<br/>
+*Image source*: www.freepik.com
+
+### Movie Source files 
+- Wikipedia data
+
+- Kaggle Metadata
+
+- The ratings data comes from Movielens, extracted from a Kaggle dataset:
+<a href="https://www.kaggle.com/rounakbanik/the-movies-dataset" target="_blank">https://www.kaggle.com/rounakbanik/the-movies-dataset</a>. The data contains over 26 million user ratings on a collection of movies.
 
 
-## Module 8 Exercises
-[movies-etl notebook](movies-etl.ipynb)
+## Automate ETL Data Pipeline
+
+Three Python classes are created for each type of source file:
+- Wikipedia_Movie
+- Kaggle
+- Ratings
+
+The extract_transform_load function in the ETL_clean_wiki_movies notebook calls methods in each of these classes. Using Python classes allows for a "separation of concerns" design so that code specific to each source file can be tested and maintained in isolation of the other source files. Also, this makes it easier to add new source files in the future as new classes to the ETL data pipeline.
+
+### Deliverable 1
+[ETL_function_test](ETL_function_test.ipynb) - Reading movie source files into Pandas DataFrames
+
+
+### Deliverable 2 
+[ETL_clean_wiki_movies](ETL_clean_wiki_movies.ipynb) - Extract and Transform Wikipedia Data
+
+
+### Deliverable 3
+[ETL_clean_kaggle_data](ETL_clean_kaggle_data.ipynb) - Extract and Transform Kaggle Data
+
+
+### Deliverable 4
+Create the Movie Database
+
 
 
 
--- movies-etl/etl.py
+++ movies-etl/etl.py
@@ -1,5 +1,5 @@
 """
-ETL module for movie data =
+ETL module for movie data -
 
 Contains methods for :
 
@@ -15,43 +15,127 @@ import numpy as np
 import os
 
 
-def extract_wikipedia_movies():
-    """
-    Extract movie data from wikipedia json file
+class Wikipedia_Movie:
+    def __init__(self, file_dir):
+        """
+        Initialize instance of Wikipedia_Movie class
+        
+        args:
+            file_dir: path to source wikipedia file
+        """
+        self._file_dir = file_dir
 
-    return: list of raw wikipedia movies
-    """
-    file_dir = os.path.join('data', 'wikipedia-movies.json')
+    
+    def read_raw_data(self):
+        """
+        Extract movie data from wikipedia json file
 
-    with open(file_dir, mode='r') as file:
-        wiki_movies_raw = json.load(file)
+        return: raw data for wikipedia movies
+        """
+        
+        with open(self._file_dir, mode='r') as file:
+            wiki_movies_raw = json.load(file)
 
-    return wiki_movies_raw
+        return wiki_movies_raw
+
+    
+    def read_data_df(self, raw_data):
+        """
+        Extract movie data from wikipedia raw data and return as DataFrame
+        
+        args:
+            raw_data: list of dictionaries containing raw wikipedia movie data 
+        
+        return: DataFrame of wikipedia movies
+        """
+        
+        return pd.DataFrame(wiki_movies_raw)
+
+    
+    def filter_tv(raw_data):
+        """
+        filter out tv shows from raw movie data
+
+        args:
+            raw_data - list of dictionaries where each dictionary entry is for a movie
+
+        return: list of transformed movies with no tv shows
+        """
+        
+        return [movie for movie in raw_data if ('Director' in movie or 'Directed by' in movie) and 'imdb_link' in movie
+                and 'No. of episodes' not in movie]
+    
+    
+    def extract_imdb_id(df):
+        """
+        Extract the IMDb ID using a regular expression string and dropping any imdb_id duplicates. 
+        If there is an error, capture and print the exception.
+        
+        args:
+            wiki movies DataFrame
+            
+        return: wiki movies DataFrame with imdb id
+        """
+        pass
+       
+        
+    def parse_dollars(s):
+        """
+        Turn extracted values into a numeric value
+
+        args:
+            s:  input value to parse against regular expression
+
+        return:
+            formatted value resulting from regex substitution
+        """
+
+        # if s is not a string, return NaN
+        if type(s) != str:
+            return np.nan
+
+        # if input is of the form $###.# million
+        if re.match(r'\$\s*\d+\.?\d*\s*milli?on', s, flags=re.IGNORECASE):
+
+            # remove dollar sign and " million"
+            s = re.sub('\$|\s|[a-zA-Z]','',s)
+
+            # convert to float and multiply by a million
+            value = float(s) * 10**6
+
+            return value
+
+        # if input is of the form $###.# billion
+        elif re.match(r'\$\s*\d+\.?\d*\s*billi?on', s, flags=re.IGNORECASE):
+
+            # remove dollar sign and " billion"
+            s = re.sub('\$|\s|[a-zA-Z]','',s)
+
+            # convert to float and multiply by a billion
+            value = float(s) * 10**9
+
+            return value
+
+        # if input is of the form $###,###,###
+        elif re.match(r'\$\s*\d{1,3}(?:[,|\.]\d{3})+(?!\s[mb]illion)', s, flags=re.IGNORECASE):
+
+            # remove dollar sign and commas
+            s = re.sub('\$|,','',s)
+
+            # convert to float
+            value = float(s)
+
+            return value
+
+        # otherwise, return NaN
+        else:
+            return np.nan
 
 
-def transform_wiki_movies(raw_data):
-    """
-    transform raw movie data
-
-    args:
-        raw_data - list of dictionaries where each dictionary entry is for a movie
-
-    return: list of transformed movies
-    """
-    return [movie for movie in raw_data if ('Director' in movie or 'Directed by' in movie) and 'imdb_link' in movie
-            and 'No. of episodes' not in movie]
-
-
-def change_column_name(old_name, new_name):
-    """
-    description: rename key called old_name to new_name
-
-    args:
-        old_name: old key name
-        new_name: new key name
-    """
-    pass
+    def wiki_file(self):
+        return self._file_dir
 
 
 if '__main__' == '__name__':
+    # file_dir = os.path.join('data', 'wikipedia-movies.json')
     pass
